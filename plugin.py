072da4aa3decca018528e1133f565088e879c3de
DerekGn <dg89327@hotmail.com>
2021-06-10 12:53:34 +0100

    Updated plugin

--- plugin.py
+++ plugin.py
@@ -243,15 +243,73 @@ class BasePlugin:
         elif(productId == Energine.PRODUCTID_AQS):
             Domoticz.Log("Creating Aqs Sensor Id: " + str(sensorId))
             Domoticz.Device(Name="AQS", DeviceID=sensorId, Unit=1,
-                            TypeName="Air Quality VOC", Type=243, SubType=31, 
+                            Type=243, Subtype=31,
                             Options={'Custom': '1;VOC Index'},
-                            Description="RfmTemp AQS").Create()
+                            Description="RfmAqs Sensor").Create()
             Domoticz.Device(Name="AQS Temp & Humidity", DeviceId=sensorId, Unit=2,
                             TypeName="Temp+Hum", Type=82,
-                            Description="RfmTemp AQS Temp & Humidity").Create()
-        # elif(productId == Energine.PRODUCTID_EM):
-        #     Domoticz.Log("Creating Energy Meter Id: " + str(sensorId))
-        #
+                            Description="RfmAqs Temp & Humidity").Create()
+        elif(productId == Energine.PRODUCTID_EM):
+            Domoticz.Log("Creating Energy Meter Id: " + str(sensorId))
+            Domoticz.Device(Name="Energy Meter Voltage", DeviceID=sensorId, Unit=1,
+                            Type=243, Subtype=31,
+                            Options={'Custom': '1;VAC'},
+                            Description="Rms Voltage").Create()
+            Domoticz.Device(Name="Energy Meter Frequency", DeviceID=sensorId, Unit=2,
+                            Type=243, Subtype=31,
+                            Options={'Custom': '1;Hz'},
+                            Description="Line Voltage Frequency").Create()
+
+            self.CreateRfmEmLineMeasurements('L', sensorId, 3)
+
+            self.CreateRfmEmLineMeasurements('n', sensorId, 9)
+
+            self.CreateRfmEmEnergyMeasurements(sensorId, 15)
+
+    def CreateRfmEmLineMeasurements(self, line, sensorId, unit):
+        Domoticz.Device(Name="Energy Meter " + line + " Current", DeviceID=sensorId, Unit=unit,
+                        Type=243, Subtype=31,
+                        Options={'Custom': '1;Irms'},
+                        Description=line + " Line Rms Current").Create()
+        Domoticz.Device(Name="Energy Meter " + line + " Phase", DeviceID=sensorId, Unit=++unit,
+                        Type=243, Subtype=31,
+                        Options={'Custom': '1;°'},
+                        Description="Phase Angle between Voltage and " + line + " Line Current").Create()
+        Domoticz.Device(Name="Energy Meter " + line + " PMean", DeviceID=sensorId, Unit=++unit,
+                        Type=243, Subtype=31,
+                        Options={'Custom': '1;kW'},
+                        Description=line + " Line Mean Active Power").Create()
+        Domoticz.Device(Name="Energy Meter " + line + " QMean", DeviceID=sensorId, Unit=++unit,
+                        Type=243, Subtype=31,
+                        Options={'Custom': '1;kvar'},
+                        Description=line + " Line Mean Reactive Power").Create()
+        Domoticz.Device(Name="Energy Meter " + line + "Power Factor", DeviceID=sensorId, Unit=++unit,
+                        Type=243, Subtype=31,
+                        Description=line + " Line Power Factor").Create()
+        Domoticz.Device(Name="Energy Meter " + line + " SMean", DeviceID=sensorId, Unit=++unit,
+                        Type=243, Subtype=31,
+                        Options={'Custom': '1;kVA'},
+                        Description=line + " Line Mean Apparent Power").Create()
+
+    def CreateRfmEmEnergyMeasurements(self, sensorId, unit):
+        Domoticz.Device(Name="Energy Meter Absolute Active Energy", DeviceID=sensorId, Unit=unit,
+                        Type=243, Subtype=28,
+                        Description="Energy Meter Absolute Active Energy").Create()
+        Domoticz.Device(Name="Energy Meter Absolute Reactive Energy", DeviceID=sensorId, Unit=++unit,
+                        Type=243, Subtype=28,
+                        Description="Energy Meter Absolute Reactive Energy").Create()
+        Domoticz.Device(Name="Energy Meter Forward Active Energy", DeviceID=sensorId, Unit=++unit,
+                        Type=243, Subtype=28,
+                        Description="Energy Meter Forward Active Energy").Create()
+        Domoticz.Device(Name="Energy Meter Forward Reactive Energy", DeviceID=sensorId, Unit=++unit,
+                        Type=243, Subtype=28,
+                        Description="Energy Meter Forward Reactive Energy").Create()
+        Domoticz.Device(Name="Energy Meter Reverse Active Energy", DeviceID=sensorId, Unit=++unit,
+                        Type=243, Subtype=28,
+                        Description="Energy Meter Reverse Active Energy").Create()
+        Domoticz.Device(Name="Energy Meter Reverse Reactive Energy", DeviceID=sensorId, Unit=++unit,
+                        Type=243, Subtype=28,
+                        Description="Energy Meter Reverse Reactive Energy").Create()
 
 global _plugin
 _plugin = BasePlugin()

56dda4dc65d782f8bb5eff26e034b88f67ec00ab
DerekGn <dg89327@hotmail.com>
2021-06-25 15:37:17 +0100

    Updated naming convention Updated plugin processing

--- Energine.py
+++ Energine.py
@@ -60,7 +60,7 @@ PRODUCTID_MIHO033                 = 0x0D # FSK: Open sensor
 CRYPT_PID                        = 242
 CRYPT_PIP                        = 0x0100
 
-def CreateDevice(sensorId, productId):
+def createDevice(sensorId, productId):
     if(productId == PRODUCTID_MIHO032):
         Domoticz.Log("Creating Motion Sensor Id: " + str(sensorId))
         Domoticz.Device(Name="Motion Sensor", DeviceID=str(sensorId), Unit=1,
@@ -72,12 +72,13 @@ def CreateDevice(sensorId, productId):
                         TypeName="Switch", Type=244, Subtype=73, Switchtype=11,
                         Description="MIHO033 Door Sensor").Create()
 
-def UpdateDevice(device, productId, message):
+def updateDevice(device, productId, message):
     if(productId == PRODUCTID_MIHO032):
         Domoticz.Log("Updating Motion Sensor Id: " + str(device.ID))
-        motionRecord = Common.FindRecord(message, OpenThings.PARAM_MOTION_DETECTOR)
-        device.Update(nValue=int(motionRecord["value"]))
+        motionRecord = Common.findRecord(message, OpenThings.PARAM_MOTION_DETECTOR)
+        #TODO map 0 value
+        device.Update(nValue=int(motionRecord["value"]),sValue=str(motionRecord["value"]))
     elif(productId == PRODUCTID_MIHO033):
         Domoticz.Log("Updating Door Sensor Id: " + str(device.ID))
-        doorRecord = Common.FindRecord(message, OpenThings.PARAM_DOOR_SENSOR)
-        device.Update(nValue=int(doorRecord["value"]))
+        doorRecord = Common.findRecord(message, OpenThings.PARAM_DOOR_SENSOR)
+        device.Update(nValue=int(doorRecord["value"]),sValue=str(motionRecord["value"]))
